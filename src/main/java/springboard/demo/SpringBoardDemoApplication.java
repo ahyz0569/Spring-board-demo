b02073a4ecc7ffbe42c623b4a39db9902b57d45f
Yunok <ahyz0569@gmail.com>
2021-01-18 03:39:39 +0900

    initial commit

---
+++ src/main/java/springboard/demo/SpringBoardDemoApplication.java
@@ -0,0 +1,13 @@
+package springboard.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoardDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoardDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springboard/demo/SpringBoardDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package springboard.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoardDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
